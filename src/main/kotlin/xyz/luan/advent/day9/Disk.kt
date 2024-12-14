c69e786c348a4350b8fa08d3b9be59014c18a033
Luan Nico <luanpotter27@gmail.com>
2024-12-13 22:52:31 -0500

    Day 9: First half

--- src/main/kotlin/xyz/luan/advent/day9/Disk.kt
+++ src/main/kotlin/xyz/luan/advent/day9/Disk.kt
@@ -1,7 +1,35 @@
 package xyz.luan.advent.day9
 
+import java.util.*
+
 class Disk {
-    fun parse(diskMap: String): String {
-        return diskMap
+    fun parse(diskMap: String): List<Block> {
+        var isFile = true
+        var fileId = 0
+
+        val result = LinkedList<Block>()
+        for (char in diskMap) {
+            val size = char.digitToInt()
+            val block = if (isFile) {
+                File(fileId++)
+            } else {
+                Space
+            }
+            repeat(size) {
+                result.add(block)
+            }
+            isFile = !isFile
+        }
+        return result
     }
+
+
+    sealed interface Block
+
+    @JvmInline
+    value class File(
+        private val id: Int,
+    ) : Block
+
+    data object Space : Block
 }
--- src/test/kotlin/xyz/luan/advent/day9/DiskTest.kt
+++ src/test/kotlin/xyz/luan/advent/day9/DiskTest.kt
@@ -7,9 +7,20 @@ class DiskTest {
     private val disk = Disk()
 
     @Test
-    fun testSum() {
-        val result = disk.parse("2333133121414131402")
-        val expected = "2333133121414131402"
+    fun test1() {
+        val result = disk.parse("12345")
+        val expected = parseExpected("0..111....22222")
         assertEquals(expected, result)
+    }
+
+    @Test
+    fun test2() {
+        val result = disk.parse("2333133121414131402")
+        val expected = parseExpected("00...111...2...333.44.5555.6666.777.888899")
+        assertEquals(expected, result)
+    }
+
+    private fun parseExpected(expected: String): List<Disk.Block> {
+        return expected.map { if (it == '.') Disk.Space else Disk.File(it.digitToInt()) }
     }
 }

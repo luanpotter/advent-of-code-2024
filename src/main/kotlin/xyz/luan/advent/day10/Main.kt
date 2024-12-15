bf89834fe57c303a463ced7f8216293d834d7c2f
Luan Nico <luanpotter27@gmail.com>
2024-12-15 00:54:26 -0500

    Implement Day 10

--- src/main/kotlin/xyz/luan/advent/day10/Main.kt
+++ src/main/kotlin/xyz/luan/advent/day10/Main.kt
@@ -1,5 +1,79 @@
 package xyz.luan.advent.day10
 
+import xyz.luan.advent.util.readFile
+
 fun main() {
-    //
+    val input = Hike.parse(readFile("day10/input"))
+
+    val hike = Hike(input)
+    println(
+        """
+        A: ${hike.computeA()}
+        B: ${hike.computeB()}
+    """.trimIndent(),
+    )
+}
+
+
+class Hike(
+    private val matrix: Matrix<Int>,
+) {
+    fun computeA(): Long {
+        val reachableLeaves = mutableMapOf<Coord, Set<Coord>>()
+
+        fun process(coord: Coord): Set<Coord> {
+            val me = matrix.get(coord)
+            if (me == 9) {
+                return setOf(coord)
+            }
+
+            val neighbors = matrix.neighborsOf(coord)
+            return neighbors
+                .filter { matrix.get(it) == me + 1 }
+                .flatMap { neighbor -> reachableLeaves.getOrPut(neighbor) { process(neighbor) } }
+                .toSet()
+        }
+
+        var total = 0L
+        for (coord in matrix.coords) {
+            if (matrix.get(coord) == 0) {
+                val leaves = reachableLeaves.getOrPut(coord) { process(coord) }
+                total += leaves.size
+            }
+        }
+        return total
+    }
+
+    fun computeB(): Long {
+        val reachableLeaves = mutableMapOf<Coord, Int>()
+
+        fun process(coord: Coord): Int {
+            val me = matrix.get(coord)
+            if (me == 9) {
+                return 1
+            }
+
+            val neighbors = matrix.neighborsOf(coord)
+            return neighbors
+                .filter { matrix.get(it) == me + 1 }
+                .sumOf { neighbor -> reachableLeaves.getOrPut(neighbor) { process(neighbor) } }
+        }
+
+        var total = 0L
+        for (coord in matrix.coords) {
+            if (matrix.get(coord) == 0) {
+                val leaves = reachableLeaves.getOrPut(coord) { process(coord) }
+                total += leaves
+            }
+        }
+        return total
+    }
+
+    companion object {
+        fun parse(input: List<String>): List<List<Int>> {
+            return input
+                .map { line -> line.trim().map { it.digitToInt() } }
+                .filter { it.isNotEmpty() }
+        }
+    }
 }
--- src/test/kotlin/xyz/luan/advent/day10/Day10Test.kt
+++ src/test/kotlin/xyz/luan/advent/day10/Day10Test.kt
@@ -5,8 +5,36 @@ import kotlin.test.assertEquals
 
 class Day10Test {
     @Test
-    fun `a test`() {
-        val result = 1 + 1
-        assertEquals(2, result)
+    fun `simplest case`() {
+        val matrix = Hike.parse(
+            """
+            0123
+            1234
+            8765
+            9876
+        """.lines(),
+        )
+        val hike = Hike(matrix)
+        assertEquals(1, hike.computeA())
+        assertEquals(16, hike.computeB())
+    }
+
+    @Test
+    fun `complex case`() {
+        val matrix = Hike.parse(
+            """
+                89010123
+                78121874
+                87430965
+                96549874
+                45678903
+                32019012
+                01329801
+                10456732
+        """.lines(),
+        )
+        val hike = Hike(matrix)
+        assertEquals(36, hike.computeA())
+        assertEquals(81, hike.computeB())
     }
 }
